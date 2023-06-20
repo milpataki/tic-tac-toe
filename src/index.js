73d5479b4490eb5f555da830af14f847514be560
Milan Pataki <milanpataki9@gmail.com>
2023-06-21 00:14:46 +0200

    squares fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-export default function Square() {
-    return <button className="square">X</button>;
-  }
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'
+import './styles.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
